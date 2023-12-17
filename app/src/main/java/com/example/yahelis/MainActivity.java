ed29db45d0279c1a0754b86c52bc4ceb94107b03
Owner <maayanov3@gmail.com>
2023-12-17 14:23:05 +0200

    yahelis

---
+++ app/src/main/java/com/example/yahelis/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yahelis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view) {
+        Intent intent=new Intent(this, signin.class);
+        startActivity(intent);
+    }
+}

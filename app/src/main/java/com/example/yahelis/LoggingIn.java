e6be43fdcc963ea41076d0c1c057e516f98d1411
Owner <maayanov3@gmail.com>
2023-12-31 16:22:32 +0200

    Added Fragment

--- app/src/main/java/com/example/yahelis/LoggingIn.java
+++ app/src/main/java/com/example/yahelis/LoggingIn.java
@@ -2,7 +2,9 @@ package com.example.yahelis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoggingIn extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class LoggingIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logging_in);
         getSupportActionBar().hide();
+    }
+
+    public void back(View view) {
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/yahelis/MainTraveling.java
@@ -0,0 +1,62 @@
+package com.example.yahelis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.SupportMenuInflater;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+
+public class MainTraveling extends FragmentActivity {
+
+    ViewPager2 viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_traveling);
+        initViews();
+
+    }
+    private void initViews() {
+        viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        FragmentPagerAdapter adapter= new FragmentPagerAdapter(this);
+
+
+        String[] arr = {"Home","FindTrip","AddTrip","Profil"};
+
+        int[] arrDraw = {R.drawable.googleg_standard_color_18,R.drawable.common_full_open_on_phone,R.drawable.send,R.drawable.user};
+        viewPager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout,viewPager,(tab, position) -> tab.setIcon(arrDraw[position])//tab.setText( arr[position])//(position + 1))
+        ).attach();
+    }
+
+}

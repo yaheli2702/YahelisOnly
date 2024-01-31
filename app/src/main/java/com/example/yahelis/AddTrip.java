363aa28adf8639e7b41aefa3abe671afae364a2a
Owner <maayanov3@gmail.com>
2024-01-31 11:38:26 +0200

    addd

--- app/src/main/java/com/example/yahelis/AddTrip.java
+++ app/src/main/java/com/example/yahelis/AddTrip.java
@@ -1,12 +1,17 @@
 package com.example.yahelis;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,8 @@ public class AddTrip extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private View view;
 
     public AddTrip() {
         // Required empty public constructor
@@ -59,6 +66,23 @@ public class AddTrip extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_trip, container, false);
+        view= inflater.inflate(R.layout.fragment_add_trip, container, false);
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button b = view.findViewById(R.id.button3);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(), addtripactivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
 }
